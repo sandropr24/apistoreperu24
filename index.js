853487a454ef03fadfc80b70e7af27660c33eaaf
Sandro Pachas Romani <sandroromanipr24@gmail.com>
2026-03-12 13:45:17 -0500

    Store peru

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express') 
+const mysql = require('mysql2')  
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'storeperu'
+})
+
+db.connect((err) => {
+  if(err) throw err;
+  console.log("Conectado a la BD de StorePeru")
+})
+
+const PORT = 3000 
+
+app.post('/productos', (req,res)=>{
+  const {idmarca, descripcion, precio, stock, garantia} = req.body
+
+  const sql = "INSERT INTO productos (idmarca, descripcion, precio, stock, garantia) VALUES (?,?,?,?,?)"
+
+  db.query(sql, [idmarca, descripcion, precio, stock, garantia], (err, results)=> {
+    if (err){
+      return res.status(500).send({
+        success: false,
+        message: 'No se concretó el registro'
+      })
+    }
+
+    res.send({
+      success: true,
+      message: 'Nuevo producto registrado',
+      id: results.insertId
+    })  
+  })
+})
+
+app.get('/productos', (req,res)=>{
+  const sql = "SELECT * FROM productos LIMIT 10"
+  db.query(sql,(err , results)=>{
+    if(err) return res.status(500).send({message: 'Error acceso a datos'})
+    res.json(results)
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})

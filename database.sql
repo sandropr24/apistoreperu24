dfee9eedd064b8dcf3806f3b0dd4f71eddfc3409
Sandro <sandroromanipr24@gmail.com>
2026-03-14 19:46:22 -0500

    Agregadas nuevas marcas

--- database.sql
+++ database.sql
@@ -24,19 +24,29 @@ CREATE TABLE  productos
 
 
 
-INSERT INTO marcas(marca)VALUES
-('Nike')
+INSERT INTO marcas(marca) VALUES
+('Nike'),
+('Adidas'),
+('Jordan'),
+('Puma'),
+('New Balance');
+
 
 INSERT INTO productos(idmarca, descripcion, precio, stock, garantia) VALUES
-(1, 'Zapatillas deportivas Nike Air Max', 120.00, 25, '6 meses'),
-(1, 'Zapatillas Nike Revolution para running', 85.50, 40, '6 meses'),
-(1, 'Polo deportivo Nike Dri-FIT', 35.90, 60, '3 meses'),
-(1, 'Short deportivo Nike entrenamiento', 29.90, 50, '3 meses'),
-(1, 'Casaca deportiva Nike con capucha', 79.90, 20, '6 meses'),
-(1, 'Mochila deportiva Nike training', 45.00, 30, '3 meses'),
-(1, 'Gorra Nike deportiva ajustable', 22.50, 70, '3 meses');
+(1, 'Zapatillas deportivas Nike Air Max', 120.00, 25, '6'),
+(1, 'Zapatillas Nike Revolution para running', 85.50, 40, '6'),
+(1, 'Polo deportivo Nike Dri-FIT', 35.90, 60, '3 '),
+(1, 'Short deportivo Nike entrenamiento', 29.90, 50, '3 '),
+(1, 'Casaca deportiva Nike con capucha', 79.90, 20, '6 '),
+(1, 'Mochila deportiva Nike training', 45.00, 30, '3 '),
+(1, 'Gorra Nike deportiva ajustable', 22.50, 70, '3 ');
 
 
 
 
 SELECT * FROM productos;
+
+SELECT * FROM marcas;
+
+
+
